c97d8132544e90fa0194d9301e126f2cecd00c44
Frostnn <gradysnn92@yandex.ru>
2022-05-26 12:00:06 +0300

    tesst

--- src/Components/MainPage/MainContent.tsx
+++ src/Components/MainPage/MainContent.tsx
@@ -112,7 +112,7 @@ const MainContent: React.FC<iMainContent> = ({ title, subTitle }) => {
             <Card img={img} titleImg={'Life as is #18'} author={'naibovisuals'} authorIcon={icon} />
           </RightContent>
         </Container>
-        <h1>TEST</h1>
+        <h1>TEST 2345</h1>
       </Wrapper>
     </>
   );
